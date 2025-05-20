5a9540745b54971bd5d335708411caf098f0af4c
aureliacitra <citrapangukir@gmail.com>
2025-05-20 10:10:54 +0700

    add if stack empty

--- stackUsingLInkendList.cpp
+++ stackUsingLInkendList.cpp
@@ -26,11 +26,21 @@ class Stack
 
     int push(int value)
     {
-     Node *newNode = new Node()
+     Node *newNode = new Node();
      newNode->data = value;
      newNode->next = top;
      top = newNode;
      cout << "Push Value: " << value << endl;
      return value;
     }
+}
+
+void pop()
+{
+    if (isEmpty())
+    {
+        cout << "stack is empty " << endl;
+    }
+
+   
 }
