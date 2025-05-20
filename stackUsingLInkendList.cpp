73898c31374d63953dd94b4f4602dfc350aea6f0
aureliacitra <citrapangukir@gmail.com>
2025-05-20 10:06:57 +0700

    add int push

--- stackUsingLInkendList.cpp
+++ stackUsingLInkendList.cpp
@@ -13,16 +13,24 @@ class Node
     }
 };
 
-class Stack;
+class Stack
 {
     private:
     Node *top;
 
     public:
-    stack()
+    Stack()
     {
-        top = *NULL;
+        top = NULL;
     }
 
-    
+    int push(int value)
+    {
+     Node *newNode = new Node()
+     newNode->data = value;
+     newNode->next = top;
+     top = newNode;
+     cout << "Push Value: " << value << endl;
+     return value;
+    }
 }
